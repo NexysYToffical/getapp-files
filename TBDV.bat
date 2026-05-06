0b40cd7fa55eb23d0cbb75eb01125fe6aa42cc39
NexysYToffical <maxiotempemail@gmail.com>
2026-05-06 11:00:09 +0000

    added a desktop enviorment

---
+++ TBDV.bat
@@ -0,0 +1,38 @@
+echo Loading text based desktop enviorment...
+echo This is a work in progress, expect bugs and missing features.
+set version=0.1
+echo Version: %version%
+echo.
+timeout /t 2 >nul
+echo Starting desktop...
+timeout /t 2 >nul
+echo.
+echo --------------------------------
+echo | Info                         |
+echo |------------------------------|
+echo |                              |
+echo |  .  This desktop enviorment  |
+echo |  |  is still in development  |
+echo |     and errors can happen.   |
+echo |                              |
+echo |        [(enter)OK}           |
+echo ---------------------------------
+choice /c:enter /n >nul
+echo Starting desktop...
+cls
+echo --------------------------------------------------------------
+echo |                                                            |
+echo | Welcome!                                                   |
+echo |                                                            |
+echo |                                                            |
+echo |                                                            |
+echo |                                                            |
+echo |                                                            |
+echo |                                                            |
+echo |                                                            |
+echo |                                                            |
+echo |                                                            |
+echo |                                                            |
+echo |                                                            |
+echo |                                                            |
+echo --------------------------------------------------------------
