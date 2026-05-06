bfc1093f14da5e81df6873860a180e4b8cc1ac91
NexysYToffical <maxiotempemail@gmail.com>
2026-05-06 13:18:58 +0100

    yaayayayay

--- TBDV.bat
+++ TBDV.bat
@@ -1,24 +1,19 @@
+@echo off
+:: [Loading sequence remains the same...]
 echo Loading text based desktop enviorment...
-echo This is a work in progress, expect bugs and missing features.
 set version=0.1
-echo Version: %version%
-echo.
-timeout /t 2 >nul
-echo Starting desktop...
-timeout /t 2 >nul
-echo.
+timeout /t 1 >nul
+
+:start_screen
+cls
 echo --------------------------------
 echo | Info                         |
 echo |------------------------------|
-echo |                              |
-echo |  .  This desktop enviorment  |
-echo |  |  is still in development  |
-echo |     and errors can happen.   |
-echo |                              |
-echo |  Press any key to continue   |
+echo | Press any key to continue    |
 echo ---------------------------------
 pause >nul
-echo Starting desktop...
+
+:main_desktop
 cls
 echo --------------------------------------------------------------
 echo |                                                            |
@@ -27,62 +22,40 @@ echo |                                                            |
 echo | 1: File Editor                                             |
 echo | 2: Software Manager                                        |
 echo |                                                            |
-echo |                                                            |
-echo |                                                            |
-echo |                                                            |
-echo |                                                            |
-echo |                                                            |
-echo |                                                            |
-echo |                                                            |
-echo |                                                            |
 echo --------------------------------------------------------------
-choice /c 1234567890 /n /m " "
-if errorlevel 1 goto fileeditor
+:: Check for HIGHEST errorlevel first
+choice /c 12 /n /m " "
 if errorlevel 2 goto softwaremanager
+if errorlevel 1 goto fileeditor
+goto main_desktop
+
 :fileeditor
 cls
-echo File Editor
-echo This is a simple file editor. It allows you to create and edit text files.
-echo.
-echo Enter the name of the file you want to edit (or create):
-set /p filename=
-if not exist "%filename%" (
-    echo File does not exist. Creating new file...
-    type nul > "%filename%"
-)
-echo Editing file: %filename%
-echo Type your text below. Press Ctrl+Z and then Enter to save and exit.
+echo --- File Editor ---
+set /p filename="File name: "
+if not exist "%filename%" type nul > "%filename%"
+echo Type text. Press Ctrl+Z then Enter to save.
 copy con "%filename%"
-goto end
+goto main_desktop
+
 :softwaremanager
 cls
-echo Software Manager
-echo This is a simple software manager. It allows you to install and uninstall software.
-echo.
-echo 1: Install Software
-echo 2: Uninstall Software
-echo 3: Back to Main Menu
+echo --- Software Manager ---
+echo 1: Install
+echo 2: Uninstall
+echo 3: Back
 choice /c 123 /n /m " "
-if errorlevel 1 goto installsoftware
+:: Again, check in descending order
+if errorlevel 3 goto main_desktop
 if errorlevel 2 goto uninstallsoftware
-if errorlevel 3 goto mainmenu
+if errorlevel 1 goto installsoftware
+
 :installsoftware
-cls
-echo Install Software
-echo This feature is not yet implemented. Please check back later.
-echo Press any key to return to the software manager.
-pause >nul
+echo Not implemented.
+pause
 goto softwaremanager
+
 :uninstallsoftware
-cls
-echo Uninstall Software
-echo This feature is not yet implemented. Please check back later.
-echo Press any key to return to the software manager.
-pause >nul
+echo Not implemented.
+pause
 goto softwaremanager
-:mainmenu
-cls
-goto start
-:end
-echo Thank you for using the text based desktop enviorment. Goodbye!
-timeout /t 2 >nul
